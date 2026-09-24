6a30fe6bd1d3103a66882b2a64bac7d122d251d2
Mateusridu <mateusridu@gmail.com>
2026-09-24 11:57:07 -0300

    correçao do link da pagina time na pagina home

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -43,7 +43,7 @@ export default function Team() {
       </div>
 
       <Link
-        to="https://www.gilbertosind.org/quem-somos"
+        to="/cartogeo/#time"
         className="mt-8 inline-flex items-center gap-2 text-sm font-semibold text-beacon hover:underline"
       >
         Conheça o time
